b436934f79cba827d7df6a8d1dd55e11c2a426ef
Sam Mullins <sammullins.co.uk@gmail.com>
2020-05-05 13:42:54 +0100

    Added XMP tag support.

--- ImageTools.Core/Selection/ExifBasedSelector.cs
+++ ImageTools.Core/Selection/ExifBasedSelector.cs
@@ -1,4 +1,9 @@
-﻿using ImageTools.Core.Builder;
+﻿using ImageMagick;
+using ImageTools.Core.Builder;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Iptc;
+using MetadataExtractor.Formats.Xmp;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -43,22 +48,56 @@ namespace ImageTools.Core.Selection
         private string GetProcessId(ImageFile imageFile)
         {
             var profile = imageFile.Image.GetExifProfile();
+            var profile2 = imageFile.Image.GetIptcProfile();
 
-            foreach(var config in _configs)
+            foreach (var config in _configs)
             {
                 var matchCategory = config.MatchProperty;
                 var matchValue = config.MatchValue;
 
                 var imageCategory = profile.Values.FirstOrDefault(v => matchCategory.Equals(v.Tag?.ToString(), StringComparison.OrdinalIgnoreCase));
-                if(string.Equals(imageCategory?.GetValue()?.ToString(), matchValue, StringComparison.OrdinalIgnoreCase))
+                var imageCategoryValue = imageCategory?.GetValue()?.ToString();
+
+                foreach (var value in profile.Values)
+                {
+                    Console.WriteLine("{0}({1}): {2}", value.Tag, value.DataType, value.ToString());
+                }
+
+                if (imageCategoryValue == null)
+                {
+                    // try to get from IPTC profile instead.
+                    var iptcCategory = profile2.Values.FirstOrDefault(v => matchCategory.Equals(Enum.GetName(typeof(IptcTag), v.Tag), StringComparison.OrdinalIgnoreCase));
+                    imageCategoryValue = iptcCategory?.Value?.ToString();
+
+                    foreach (var value in profile2.Values)
+                    {
+                        Console.WriteLine("{0}({1}, {2}): {3}", value.Tag, Enum.GetName(typeof(IptcTag), value.Tag), value.Value, value.ToString());
+                    }
+                }
+                else
+                {
+                    var lowerMatchKey = matchCategory.ToLower();
+
+                    var metadata = ImageMetadataReader.ReadMetadata(imageFile.Path);
+                    var xmpDirectory = metadata.OfType<XmpDirectory>().FirstOrDefault();
+                    var xmpDictionary = xmpDirectory.GetXmpProperties().ToDictionary(x => x.Key.ToLower(), x => x.Value);
+
+                    if (imageCategoryValue == null && xmpDictionary.ContainsKey(lowerMatchKey))
+                    {
+                        // try to get from XMP profile instead.
+                        imageCategoryValue = xmpDictionary[lowerMatchKey];
+                    }
+
+                    foreach (var value in profile2.Values)
+                    {
+                        Console.WriteLine("{0}({1}, {2}): {3}", value.Tag, Enum.GetName(typeof(IptcTag), value.Tag), value.Value, value.ToString());
+                    }
+                }
+
+                if (string.Equals(imageCategoryValue, matchValue, StringComparison.OrdinalIgnoreCase))
                 {
                     return config.Id;
                 }
-            }
-
-            foreach (var value in profile.Values)
-            {
-                Console.WriteLine("{0}({1}): {2}", value.Tag, value.DataType, value.ToString());
             }
 
             return null;
